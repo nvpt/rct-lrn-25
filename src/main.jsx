f007648226246fe3df04310c3b18a515d8d5fa71
zato.prosto <zato.prosto@ya.ru>
2025-10-06 14:46:44 +0300

    hw-2: код-ревью правки

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { restaurants } from './../public/mock'
-
-const RESTOURANTS = restaurants
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { restaurants } from './../public/mock';
 
 const rootElement = document.getElementById('root');
-if (rootElement != null) {
 
-  // @ts-ignore
-  createRoot(document.getElementById('root')).render(
-    <>
-      <h1>Рестораны</h1>
-      <hr />
-      <div>
-        {
-          RESTOURANTS.map((rest, i1) => {
-            return (
-              <div key={ i1 }>
-                <h2 > Ресторан "{ rest.name }"</h2>
-                <h3 > Меню</h3>
-                <ul>
-                  { rest.menu.map((dish, i2) => {
-                    return (
-                      <li key={ i2 }>{ dish.name }</li>
-                    )
-                  }) }
-                </ul>
-                <h3>Отзывы</h3>
-                <ul>
-                  { rest.reviews.map((resp, i3) => {
-                    return (
-                      <li key={ i3 }><i>{ resp.user }: { resp.text }</i></li>
-                    )
-                  }) }
-                </ul>
-                <br />
-                <br />
-              </div>
-            )
-          })
-        }
-      </div >
-    </>
-  )
-
-}
+createRoot(document.getElementById('root')).render(
+  <>
+    <h1>Рестораны</h1>
+    <hr />
+    <div>
+      {restaurants.map((restaurant) => {
+        return (
+          <div key={restaurant.id}>
+            <h2> Ресторан "{rest.name}"</h2>
+            <h3> Меню</h3>
+            <ul>
+              {restaurant.menu.map((dish) => {
+                return <li key={dish.id}>{dish.name}</li>;
+              })}
+            </ul>
+            <h3>Отзывы</h3>
+            <ul>
+              {restaurant.reviews.map((review) => {
+                return (
+                  <li key={review.id}>
+                    <i>
+                      {review.user}: {review.text}
+                    </i>
+                  </li>
+                );
+              })}
+            </ul>
+            <br />
+            <br />
+          </div>
+        );
+      })}
+    </div>
+  </>
+);
