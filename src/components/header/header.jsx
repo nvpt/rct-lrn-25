11556ef4c585f81e5461f5a84ffdeaeafcf2ce3f
zato.prosto <zato.prosto@ya.ru>
2025-10-19 23:57:52 +0300

    hw-6: добавлен контекст авторизации и условие отображения компонентов в зависимости от авториизации

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,9 +4,16 @@ import {
   THEME_CONTEXT_LIST,
   ThemeContext,
 } from '../../providers/theme-provider';
+import { AuthContext } from '../../providers/auth-provider';
+import { Button } from '../button/button';
+
+const someUser = {
+  name: 'Zato',
+};
 
 export const Header = () => {
   const { theme, changeTheme } = useContext(ThemeContext);
+  const { user, changeAuthorize } = useContext(AuthContext);
   const themeOptions = useRef(Object.values(THEME_CONTEXT_LIST));
 
   /**
@@ -21,15 +28,28 @@ export const Header = () => {
     <header className={cn.header}>
       <h2>HEADER</h2>
 
-      <div className={cn.themeSelector}>
-        <label htmlFor='themeSelector'>Выбрать тему:</label>
-        <select id='themeSelector' value={theme} onChange={handleChangeTheme}>
-          {themeOptions.current.map((option) => (
-            <option value={option} key={option}>
-              {option}
-            </option>
-          ))}
-        </select>
+      <div className={cn.rightPart}>
+        <div className={cn.user}>
+          {user?.name && (
+            <>
+              {user.name}
+              <Button title={'Выйти'} onClick={() => changeAuthorize(null)} />
+            </>
+          )}
+          {!user && (
+            <Button title={'Войти'} onClick={() => changeAuthorize(someUser)} />
+          )}
+        </div>
+        <div className={cn.themeSelector}>
+          <label htmlFor='themeSelector'>Выбрать тему:</label>
+          <select id='themeSelector' value={theme} onChange={handleChangeTheme}>
+            {themeOptions.current.map((option) => (
+              <option value={option} key={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
     </header>
   );
