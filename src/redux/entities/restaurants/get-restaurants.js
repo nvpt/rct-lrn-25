94c5bb4c8762373bc30f5547a6b3034517d7c699
zato.prosto <zato.prosto@ya.ru>
2025-11-06 00:18:15 +0300

    hw-9: данные по всем ресторанам загружаются при условии, что они еще не загружены

---
+++ src/redux/entities/restaurants/get-restaurants.js
@@ -0,0 +1,26 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { BASE_URL } from '../../../constants/api-const';
+import { selectRestaurantsIds } from './restaurants-slice';
+
+export const getRestaurants = createAsyncThunk(
+  'restaurants/getRestaurants',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await fetch(`${BASE_URL}/restaurants`);
+      const result = await response.json();
+
+      if (!result?.length) {
+        return rejectWithValue('Список ресторанов не получен.');
+      }
+
+      return result;
+
+      // todo здесь тоже отработает rejected?
+    } catch (err) {
+      console.log('get-restaurants.js 10 >>> err:', err);
+    }
+  },
+  {
+    condition: (_, { getState }) => !selectRestaurantsIds(getState())?.length,
+  }
+);
